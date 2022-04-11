f37bae753025c71df60003204097101800d961fa
AdrianSzklarski <szklarski.adrian@gmail.com>
2022-04-11 13:01:11 +0200

    Sum and Average value, add added functionality, fault tolerance

--- Sum_and_Average_value.py
+++ Sum_and_Average_value.py
@@ -1,17 +1,43 @@
-n = int(input("Podaj ile liczb chcesz wprowadzić?: "))
-var=0
-numbers = []
-while var < n:
-	my_no=int(input("Podaj liczbe: "))
-	var+=1
-	numbers.append(my_no)
-print(numbers)
-print("Suma elementów na liście: ",sum(numbers))
-print("Średnia wartość elentów z listy: ",sum(numbers)/n)
-if sum(numbers) > sum(numbers)/n:
-	print("Suma jest wieksza do średniej!")
-else:
-	print("Suma nie jest większa od średniej")
-	
-# Uwaga: To zadanie sprawiło mi najwięcej problemów, tzn. wymiślenie
-# rozwiązania linie 4 - 7, zwłaszcza 7.	
+"""     Adrian SZKLARSKI, 12.2021r,
+          Sum and Average value:
+
+Numbers program: the user specifies the length of an array,
+then fills it with numbers, and the program returns their
+sum and average value
+
+    Returns:
+    Parameters:
+    Error:
+"""
+
+
+def numbers(n):
+    var = 0
+    numbers_list = []
+    while var < n:
+        print(var, end=": ")
+        my_no = (int(input('Specify the value of an array element: ')))
+        a = numbers_list.append(my_no)
+        var += 1
+    if sum(numbers_list) > (sum(numbers_list) / n):
+        print(f'The sum is greater than the average!')
+    else:
+        print(f'The sum is not greater than the average')
+    return numbers_list
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            n = int(input("How many numbers you want to enter?: "))
+            print(' ')
+            break
+        except ValueError or NameError:
+            print("You should have given either an int")
+            print(' ')
+
+result = (numbers(n))
+print(' ')
+print('List of elements: ', result)
+print('Sum of elements in the list:', sum(result))
+print('Average value of electives on the list:', sum(result) / n)
